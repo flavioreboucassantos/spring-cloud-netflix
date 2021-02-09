cbc994164f2bf658e72e44a58914e172a2d1c2f5
Flávio Rebouças Santos <flavioreboucassantos@gmail.com>
2021-02-09 20:25:30 -0300

    Create Microservice CRUD

---
+++ crud/src/main/java/com/flavioreboucassantos/crud/controller/ProdutoController.java
@@ -0,0 +1,106 @@
+package com.flavioreboucassantos.crud.controller;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flavioreboucassantos.crud.data.vo.ProdutoVO;
+import com.flavioreboucassantos.crud.service.ProdutoService;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+	private final ProdutoService produtoService;
+	private final PagedResourcesAssembler<ProdutoVO> assembler;
+
+	@Autowired
+	public ProdutoController(ProdutoService produtoService, PagedResourcesAssembler<ProdutoVO> assembler) {
+		super();
+		this.produtoService = produtoService;
+		this.assembler = assembler;
+	}
+
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public ProdutoVO findById(@PathVariable("id") Long id) {
+		ProdutoVO produtoVO = produtoService.findById(id);
+		produtoVO.add(
+				linkTo(
+						methodOn(ProdutoController.class).findById(id))
+								.withSelfRel());
+		return produtoVO;
+	}
+
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public ResponseEntity<?> findAll(
+			@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "12") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "nome"));
+
+		Page<ProdutoVO> produtos = produtoService.findAll(pageable);
+		produtos.stream()
+				.forEach(produto -> {
+					produto.add(
+							linkTo(
+									methodOn(ProdutoController.class).findById(produto.getId()))
+											.withSelfRel());
+				});
+
+		PagedModel<EntityModel<ProdutoVO>> pagedModel = assembler.toModel(produtos);
+
+		return new ResponseEntity<>(pagedModel, HttpStatus.OK);
+	}
+
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public ProdutoVO create(@RequestBody ProdutoVO produtoVO) {
+		ProdutoVO retorno = produtoService.create(produtoVO);
+		retorno.add(
+				linkTo(
+						methodOn(ProdutoController.class).findById(retorno.getId()))
+								.withSelfRel());
+		return retorno;
+	}
+	
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public ProdutoVO update(@RequestBody ProdutoVO produtoVO) {
+		ProdutoVO retorno = produtoService.update(produtoVO);
+		retorno.add(
+				linkTo(
+						methodOn(ProdutoController.class).findById(retorno.getId()))
+								.withSelfRel());
+		return retorno;
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		produtoService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
+}
